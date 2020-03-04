118faaa51062fe092ebce05d4582a771824bf73b
Kazuma Takata <neilbuzztakata@gmail.com>
2020-03-04 20:01:23 +0900

    add plus evaluator

--- eval.lisp
+++ eval.lisp
@@ -1,33 +1,25 @@
 
-(defun eval-terminal (token)
-  (let ((type (getf token :type))
-	(value (getf token :value)))
-    (cond
-     ((string= type "NUMBER")
-      (parse-integer value)))))
-
-
-(defun eval-op (ast-data)
-  (let ((op-token (getf (first ast-data)
-			:value))
-	(operand-1 (second ast-data))
-	(operand-2 (third ast-data)))
-    (progn
-      (if (some #'listp operand-1)
-	  (setq operand-1 (eval-ast operand-1))
-	(setq operand-1 (eval-terminal operand-1)))
-      (if (some #'listp operand-2)
-	  (setq operand-2 (eval-ast operand-2))
-	(setq operand-2 (eval-terminal operand-2)))
-      (cond
-       ((string= op-token "PLUS")
-	(+ operand-1 operand-2))
-       ((string= op-token "MINUS")
-	(- operand-1 operand-2))))))
-
-(defun eval-ast (ast-data)
-  (let ((type (getf (first ast-data)
-		    :type)))
+(defun eval-tree (ast-data)
+  (let ((type (getf (first ast-data) :type)))
     (cond
      ((string= type "OP")
-      (eval-op ast-data)))))
+      (let ((operator (getf (first ast-data) :value)))
+           (let ((ast-data (rest ast-data)) (accum 0))
+             (progn (loop do 
+                   (progn (let ((operand-node (first ast-data)) (operand nil))
+                           (progn (cond ((string= (getf operand-node :type) "NESTED") 
+                                         (setq operand (eval-tree (getf operand-node :value))))
+                                        ((string= (getf operand-node :type) "NUMBER")
+                                         (setq operand (parse-integer (getf operand-node :value)))))
+                                  (cond ((string= operator "PLUS")
+                                         (setq accum (+ accum operand)))
+                                        ((string= operator "MINUS")
+                                         (setq accum (- accum operand)))
+                                        )
+                            )) 
+                          (setf ast-data (rest ast-data)))
+                    while ast-data)
+                    accum)
+             )
+
+        )))))
--- script.lisp
+++ script.lisp
@@ -1,6 +1,9 @@
 (load (compile-file "lexer.lisp"))
 (load (compile-file "parser.lisp"))
+(load (compile-file "eval.lisp"))
 
 
-;(defvar input (create-lex-data "(+ (- 1 10) 1)"))
-;(defvar tree (parse input))
+
+(defvar input (create-lex-data "(+ (- 1 3) 1)"))
+(defvar tree (parse_eat_lparen input))
+
